bdd398e92a417943232b0a8aa1f4441a01229e77
jkl <189156395@qq.com>
2020-12-27 19:44:43 +0800

    分页查询

--- guigu_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ guigu_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -1,13 +1,17 @@
 package com.atguigu.eduservice.controller;
 
 
+import com.atguigu.commonutils.R;
 import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.entity.vo.TeacherQuery;
 import com.atguigu.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +23,7 @@ import java.util.List;
  * @author testjava
  * @since 2020-12-22
  */
+@Api(description = "讲师管理")
 @RestController
 @RequestMapping("/eduservice/edu-teacher")
 public class EduTeacherController {
@@ -26,9 +31,64 @@ public class EduTeacherController {
     @Autowired
     private EduTeacherService eduTeacherService;
 
+    @ApiOperation(value = "所有讲师列表")
     @GetMapping("findAll")
-    public List<EduTeacher> findAllTeacher(){
-        return  eduTeacherService.list(null);
+    public R findAllTeacher() {
+        List<EduTeacher> list = eduTeacherService.list(null);
+        return R.ok().data("items", list);
     }
+
+    @ApiOperation(value = "逻辑删除讲师")
+    @DeleteMapping("{id}")
+    public R removeTeacher(@PathVariable String id) {
+        boolean flag = eduTeacherService.removeById(id);
+        if (flag) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
+    }
+
+    @GetMapping("pageTeacher/{current}/{limit}")
+    public R pageListTeacher(@PathVariable long current, @PathVariable long limit) {
+        Page<EduTeacher> pageTeacher = new
+                Page<>(current, limit);
+        eduTeacherService.page(pageTeacher, null);
+        long total = pageTeacher.getTotal();
+        List<EduTeacher> records = pageTeacher.getRecords();
+        return R.ok().data("total", total).data("rows", records);
+    }
+
+    @PostMapping("pageTeacherCondition/{current}/{limit}")
+    public R pageTeacherCondition(
+            @PathVariable long current,
+            @PathVariable long limit,
+            @RequestBody(required=false) TeacherQuery teacherQuery) {
+        Page<EduTeacher> pageTeacher = new Page<>(current, limit);
+        QueryWrapper<EduTeacher> wrapper = new QueryWrapper<>();
+        String name = teacherQuery.getName();
+        String begin = teacherQuery.getBegin();
+        Integer level = teacherQuery.getLevel();
+        String end = teacherQuery.getEnd();
+
+        if (!StringUtils.isEmpty(name)) {
+            wrapper.like("name", name);
+        }
+        if (!StringUtils.isEmpty(level)){
+            wrapper.eq("level", level);
+        }
+        if (!StringUtils.isEmpty(begin)) {
+            wrapper.ge("gmt_create", begin);
+        }
+        if (!StringUtils.isEmpty(end)) {
+            wrapper.le("gmt_create", end);
+        }
+
+        eduTeacherService.page(pageTeacher, wrapper);
+        long total = pageTeacher.getTotal();
+        List<EduTeacher> records = pageTeacher.getRecords();
+        return R.ok().data("total", total).data("rows", records);
+    }
+
 }
 
