e777b4a19811103b6724b8d4e3eaa37a5125d2ad
jkl <189156395@qq.com>
2021-01-10 15:40:42 +0800

    添加课程分类和课程分类列表

--- guigu_parent/service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduSubjectServiceImpl.java
+++ guigu_parent/service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduSubjectServiceImpl.java
@@ -3,14 +3,21 @@ package com.atguigu.eduservice.service.impl;
 import com.alibaba.excel.EasyExcel;
 import com.atguigu.eduservice.entity.EduSubject;
 import com.atguigu.eduservice.entity.excel.SubjectData;
+import com.atguigu.eduservice.entity.subject.OneSubject;
+import com.atguigu.eduservice.entity.subject.TwoSubject;
 import com.atguigu.eduservice.listener.SubjectExcelListener;
 import com.atguigu.eduservice.mapper.EduSubjectMapper;
 import com.atguigu.eduservice.service.EduSubjectService;
+import com.atguigu.servicebase.exceptionhandler.GuliException;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -28,8 +35,40 @@ public class EduSubjectServiceImpl extends ServiceImpl<EduSubjectMapper, EduSubj
         try {
             InputStream in = file.getInputStream();
             EasyExcel.read(in, SubjectData.class, new SubjectExcelListener(eduSubjectService)).sheet().doRead();
-        }catch (Exception e){
-
+        } catch (Exception e) {
+            throw new GuliException(20001, "读取文件错误！！！！");
         }
+    }
+
+    @Override
+    public List<OneSubject> getAllOneTwoSubject() {
+        QueryWrapper<EduSubject> wrapperOne = new QueryWrapper<>();
+        wrapperOne.eq("parent_id", "0");
+        List<EduSubject> oneSubjectList = baseMapper.selectList(wrapperOne);
+
+        QueryWrapper<EduSubject> wrapperTwo = new QueryWrapper<>();
+        wrapperTwo.ne("parent_id", "0");
+        List<EduSubject> twoSubjectList = baseMapper.selectList(wrapperTwo);
+
+        List<OneSubject> finalSubjectList = new ArrayList<>();
+
+        for (EduSubject eduSubject : oneSubjectList) {
+            OneSubject oneSubject = new OneSubject();
+            BeanUtils.copyProperties(eduSubject, oneSubject);
+            finalSubjectList.add(oneSubject);
+
+            // 二级分类进行封装
+            List<TwoSubject> twoFinalSubjectList = new ArrayList<>();
+            for (EduSubject tSubject : twoSubjectList) {
+                if (tSubject.getParentId().equals(eduSubject.getId())){
+                    TwoSubject twoSubject = new TwoSubject();
+                    BeanUtils.copyProperties(tSubject, twoSubject);
+                    twoFinalSubjectList.add(twoSubject);
+                }
+            }
+            oneSubject.setChildren(twoFinalSubjectList);
+        }
+
+        return finalSubjectList;
     }
 }
