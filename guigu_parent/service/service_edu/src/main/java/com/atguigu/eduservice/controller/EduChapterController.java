f0a2dd6e388e9448c7c5f45a7fad81b63a19dd8a
jkl <189156395@qq.com>
2021-01-16 21:29:38 +0800

    章节添加删除操作

--- guigu_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduChapterController.java
+++ guigu_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduChapterController.java
@@ -2,6 +2,7 @@ package com.atguigu.eduservice.controller;
 
 
 import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.EduChapter;
 import com.atguigu.eduservice.entity.chapter.ChapterVo;
 import com.atguigu.eduservice.service.EduChapterService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,34 @@ public class EduChapterController {
     public R getChapterVideo(@PathVariable String courseId) {
         List<ChapterVo> list = eduChapterService.getChapterVideoByCourseId(courseId);
         return R.ok().data("allChapterVideo", list);
+    }
+
+    @PostMapping("addChapter")
+    public R addChapter(@RequestBody EduChapter eduChapter) {
+        eduChapterService.save(eduChapter);
+        return R.ok();
+    }
+
+    @GetMapping("getChapterInfo/{chapterId}")
+    public R getChapterInfo(@PathVariable String chapterId) {
+        EduChapter eduChapter = eduChapterService.getById(chapterId);
+        return R.ok().data("chapter", eduChapter);
+    }
+
+    @PostMapping("updateChapter")
+    public R updateChapter(@RequestBody EduChapter eduChapter) {
+        eduChapterService.updateById(eduChapter);
+        return R.ok();
+    }
+
+    @DeleteMapping("{chapterId}")
+    public R deleteChapter(@PathVariable String chapterId) {
+        boolean flag = eduChapterService.deleteChapter(chapterId);
+        if (!flag) {
+            return R.error();
+        } else {
+            return R.ok();
+        }
     }
 }
 
--- guigu_parent/service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduChapterServiceImpl.java
+++ guigu_parent/service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduChapterServiceImpl.java
@@ -7,6 +7,7 @@ import com.atguigu.eduservice.entity.chapter.VideoVo;
 import com.atguigu.eduservice.mapper.EduChapterMapper;
 import com.atguigu.eduservice.service.EduChapterService;
 import com.atguigu.eduservice.service.EduVideoService;
+import com.atguigu.servicebase.exceptionhandler.GuliException;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.BeanUtils;
@@ -63,5 +64,18 @@ public class EduChapterServiceImpl extends ServiceImpl<EduChapterMapper, EduChap
         }
 
         return finalList;
+    }
+
+    @Override
+    public boolean deleteChapter(String chapterId) {
+        QueryWrapper<EduVideo> wrapper = new QueryWrapper<>();
+        wrapper.eq("chapter_id", chapterId);
+        int count = eduVideoService.count(wrapper);
+        if (count > 0) {
+            throw new GuliException(20001, "不能删除");
+        } else {
+            int result = baseMapper.deleteById(chapterId);
+            return result > 0;
+        }
     }
 }
