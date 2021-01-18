6bb797c174351c0862dc59f781d4806241fe8754
jkl <189156395@qq.com>
2021-01-19 02:11:59 +0800

    课程列表页面分页展示

--- guigu_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduCourseController.java
+++ guigu_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduCourseController.java
@@ -2,11 +2,19 @@ package com.atguigu.eduservice.controller;
 
 
 import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.EduCourse;
 import com.atguigu.eduservice.entity.vo.CourseInfoVo;
 import com.atguigu.eduservice.entity.vo.CoursePublishVo;
+import com.atguigu.eduservice.entity.vo.CourseQuery;
 import com.atguigu.eduservice.service.EduCourseService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * <p>
@@ -51,6 +59,45 @@ public class EduCourseController {
     public R getPublishCourseInfo(@PathVariable String courseId) {
         CoursePublishVo coursePublishVo = eduCourseService.publishCourseInfo(courseId);
         return R.ok().data("publishCourse", coursePublishVo);
+    }
+
+
+    // 课程列表， 基本实现
+    @GetMapping
+    public R getCourseList() {
+        List<EduCourse> list = eduCourseService.list(null);
+        return R.ok().data("list", list);
+    }
+
+
+    // 完成条件查询带分页
+    @PostMapping("pageCourseCondition/{current}/{limit}")
+    public R pageCourseCondition(
+            @PathVariable long current,
+            @PathVariable long limit,
+            @RequestBody(required = false) CourseQuery courseQuery) {
+
+        Page<EduCourse> pageCourse = new Page<>(current, limit);
+        QueryWrapper<EduCourse> wrapper = new QueryWrapper<>();
+        String title = courseQuery.getTitle();
+        String status = courseQuery.getStatus();
+
+        if (!StringUtils.isEmpty(title)){
+            wrapper.like("title", title);
+        }
+        if (!StringUtils.isEmpty(status)){
+            wrapper.eq("status", status);
+        }
+
+        wrapper.orderByDesc("gmt_create");
+
+        eduCourseService.page(pageCourse, wrapper);
+
+        long total = pageCourse.getTotal();
+
+        List<EduCourse> records = pageCourse.getRecords();
+
+        return R.ok().data("total", total).data("rows", records);
     }
 }
 
