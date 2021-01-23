0e2bf86cbea03b823305273975f6b1ac0cd5319d
jiangkaili <189156395@qq.com>
2021-01-24 00:03:40 +0800

    nacos 配置

--- guigu_parent/service/service_edu/src/main/java/com/atguigu/eduservice/EduApplication.java
+++ guigu_parent/service/service_edu/src/main/java/com/atguigu/eduservice/EduApplication.java
@@ -2,9 +2,11 @@ package com.atguigu.eduservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan(basePackages = {"com.atguigu"})
 public class EduApplication {
     public static void main(String[] args) {
