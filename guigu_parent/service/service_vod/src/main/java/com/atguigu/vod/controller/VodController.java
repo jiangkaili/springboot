bba6869cd7257a3e27b9d73229fb1783efeba020
jkl <189156395@qq.com>
2021-01-23 17:01:41 +0800

    视频删除

--- guigu_parent/service/service_vod/src/main/java/com/atguigu/vod/controller/VodController.java
+++ guigu_parent/service/service_vod/src/main/java/com/atguigu/vod/controller/VodController.java
@@ -1,12 +1,14 @@
 package com.atguigu.vod.controller;
 
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.vod.model.v20170321.DeleteVideoRequest;
 import com.atguigu.commonutils.R;
+import com.atguigu.servicebase.exceptionhandler.GuliException;
+import com.atguigu.vod.Utils.ConstantVodUtils;
+import com.atguigu.vod.Utils.InitVodClient;
 import com.atguigu.vod.service.VodService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -17,11 +19,23 @@ public class VodController {
     @Autowired
     private VodService vodService;
 
-    //上传视频到阿里云
     @PostMapping("uploadAlyiVideo")
     public R uploadAlyiVideo(MultipartFile file) {
-        //返回上传视频id
         String videoId = vodService.uploadVideoAly(file);
         return R.ok().data("videoId",videoId);
+    }
+
+    @DeleteMapping("removeAlyVideo/{videoId}")
+    public R removeAlyVideo(@PathVariable String videoId) {
+        try {
+            DefaultAcsClient client = InitVodClient.initVodClient(ConstantVodUtils.ACCESS_KEY_ID, ConstantVodUtils.ACCESS_KEY_SECRET);
+            DeleteVideoRequest request = new DeleteVideoRequest();
+            request.setVideoIds(videoId);
+            client.getAcsResponse(request);
+            return R.ok();
+        }catch (Exception e) {
+            e.printStackTrace();
+            throw new GuliException(20001, "删除视频失败");
+        }
     }
 }
