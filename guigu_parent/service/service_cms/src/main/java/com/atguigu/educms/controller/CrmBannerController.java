f8ba072fd11027d9fbc3d41e6b6dc9f7eb08a6bc
jkl <189156395@qq.com>
2021-02-03 14:42:14 +0800

    cms模块banner

---
+++ guigu_parent/service/service_cms/src/main/java/com/atguigu/educms/controller/CrmBannerController.java
@@ -0,0 +1,24 @@
+package com.atguigu.educms.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 首页banner表 前端控制器
+ * </p>
+ *
+ * @author jkl
+ * @since 2021-02-02
+ */
+@RestController
+@RequestMapping("/educms/crm-banner")
+@CrossOrigin
+public class CrmBannerController {
+
+
+}
+
---
+++ guigu_parent/service/service_cms/src/main/java/com/atguigu/educms/service/CrmBannerService.java
@@ -0,0 +1,16 @@
+package com.atguigu.educms.service;
+
+import com.atguigu.educms.entity.CrmBanner;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 首页banner表 服务类
+ * </p>
+ *
+ * @author jkl
+ * @since 2021-02-02
+ */
+public interface CrmBannerService extends IService<CrmBanner> {
+
+}
