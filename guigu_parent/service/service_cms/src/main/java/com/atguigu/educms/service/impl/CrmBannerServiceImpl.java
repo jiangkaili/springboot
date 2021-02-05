87bc8a2cb92d38ab1408c60f13b93143febc304b
jkl <189156395@qq.com>
2021-02-05 14:20:10 +0800

    首页和redis完成

--- guigu_parent/service/service_cms/src/main/java/com/atguigu/educms/service/impl/CrmBannerServiceImpl.java
+++ guigu_parent/service/service_cms/src/main/java/com/atguigu/educms/service/impl/CrmBannerServiceImpl.java
@@ -3,8 +3,11 @@ package com.atguigu.educms.service.impl;
 import com.atguigu.educms.entity.CrmBanner;
 import com.atguigu.educms.mapper.CrmBannerMapper;
 import com.atguigu.educms.service.CrmBannerService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
 
 import java.util.List;
 
@@ -19,8 +22,13 @@ import java.util.List;
 @Service
 public class CrmBannerServiceImpl extends ServiceImpl<CrmBannerMapper, CrmBanner> implements CrmBannerService {
 
+
+    @Cacheable(value = "banner", key = "'selectIndexList'")
     @Override
     public List<CrmBanner> selectAllBanner() {
+        QueryWrapper<Object> wrapper = new QueryWrapper<>();
+        wrapper.orderByDesc("id");
+        wrapper.last("limit 2");
         List<CrmBanner> list = baseMapper.selectList(null);
         return list;
     }
