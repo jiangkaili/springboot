4e9d4d49cd298e18836a63569ed45c43d7ba2a33
jkl <189156395@qq.com>
2021-02-06 18:03:27 +0800

    token获取用户信息

--- guigu_parent/service/service_ucenter/src/main/java/com/atguigu/educenter/controller/UcenterMemberController.java
+++ guigu_parent/service/service_ucenter/src/main/java/com/atguigu/educenter/controller/UcenterMemberController.java
@@ -1,12 +1,15 @@
 package com.atguigu.educenter.controller;
 
 
+import com.atguigu.commonutils.JwtUtils;
 import com.atguigu.commonutils.R;
 import com.atguigu.educenter.entity.UcenterMember;
 import com.atguigu.educenter.entity.vo.RegisterVo;
 import com.atguigu.educenter.service.UcenterMemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
@@ -37,6 +40,14 @@ public class UcenterMemberController {
     public R registerUser(@RequestBody RegisterVo registerVo) {
         memberService.register(registerVo);
         return R.ok();
+    }
+
+    // 根据token获取用户信息
+    @GetMapping("getMemberInfo")
+    public R getMemberInfo(HttpServletRequest request) {
+        String memberId = JwtUtils.getMemberIdByJwtToken(request);
+        UcenterMember member = memberService.getById(memberId);
+        return R.ok().data("userInfo", member);
     }
 }
 
