9b28cebabde6ccb2fb462c9c7ff41e77c2045f97
jkl <189156395@qq.com>
2021-02-16 15:36:16 +0800

    微信支付完成

--- guigu_parent/service/service_order/src/main/java/com/atguigu/eduorder/client/UcenterClient.java
+++ guigu_parent/service/service_order/src/main/java/com/atguigu/eduorder/client/UcenterClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Component
-@FeignClient("service-edu")
+@FeignClient("service-ucenter")
 public interface UcenterClient {
 
     @PostMapping("/educenter/member/getUserInfo/{id}")
--- guigu_parent/service/service_order/src/main/java/com/atguigu/eduorder/controller/PayLogController.java
+++ guigu_parent/service/service_order/src/main/java/com/atguigu/eduorder/controller/PayLogController.java
@@ -1,9 +1,12 @@
 package com.atguigu.eduorder.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.atguigu.commonutils.R;
+import com.atguigu.eduorder.service.PayLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 /**
  * <p>
@@ -14,8 +17,36 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-02-15
  */
 @RestController
-@RequestMapping("/eduorder/pay-log")
+@RequestMapping("/eduorder/paylog")
+@CrossOrigin
 public class PayLogController {
 
+    @Autowired
+    private PayLogService payLogService;
+
+
+    // 生成微信支付二维码的接口
+    @GetMapping("createNative/{orderNo}")
+    public R createNative(@PathVariable String orderNo) {
+        Map map = payLogService.createNative(orderNo);
+        System.out.println("***********返回二维码map集合" + map);
+        return R.ok().data(map);
+    }
+
+
+    // 查询订单支付的状态
+    @GetMapping("queryPayStatus/{orderNo}")
+    public R queryPayStatus(@PathVariable String orderNo) {
+        Map<String, String> map = payLogService.queryPayStatus(orderNo);
+        if (map==null) {
+            return R.error().message("支付出错了");
+        }
+
+        if (map.get("trade_state").equals("SUCCESS")){
+            payLogService.updateOrdersStatus(map);
+            return R.ok().message("支付成功");
+        }
+        return R.ok().code(25000).message("支付中");
+    }
 }
 
